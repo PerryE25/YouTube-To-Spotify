eede9d6fcc0d48ac6ef636c042311a180bae9821
PerryE25 <perryehimuh@gmail.com>
2023-06-23 18:38:35 -0500

    Day One complete (shoutout to my day ones!)

---
+++ src/secret.py
@@ -0,0 +1,10 @@
+# Contain info to be used for the program but not to be seen
+# by the public (need authentification stuff and it's sensitive info)
+# like Spotify username and passwords
+
+# Contain info to be use for our programm ... but not to be seen by the public
+
+YOUTUBE_PLAYLIST_ID = ""
+SPOTIFY_CLIENT_ID = "9508e419b25b4e9a85be339181d1373c"
+SPOTIFY_CLIENT_SECRET = "4687573a62c246788bab8f5db9f788c8"
+SPOTIFY_USER_ID = "31rl4azkpx7rssgixhqvan2voai4"
